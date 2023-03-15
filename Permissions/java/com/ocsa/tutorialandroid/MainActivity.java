647aec2bbc459fc4d10c9a5375b880c55cc9be43
pratyaksaocsa <pratyaksa.ocsa@gmail.com>
2023-03-15 11:57:59 +0700

    Change deprecated function

--- Permissions/java/com/ocsa/tutorialandroid/MainActivity.java
+++ Permissions/java/com/ocsa/tutorialandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ocsa.tutorialandroid;
 
-import androidx.annotation.Nullable;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -8,57 +9,58 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int SHOW_CONTACT = 1;
-    private final int PERMISSION_REQ_CONTACT = 2;
+    private final int READ_CONTACTS_PERMISSION = 2;
+    private ActivityResultLauncher<Intent> activityResultLaunch;
+
+    private Button showContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] permissions = {Manifest.permission.READ_CONTACTS};
-        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
-            for(String permission: permissions) {
-                if(ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQ_CONTACT);
+        String[] PERMISSIONS = {Manifest.permission.READ_CONTACTS};
+        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)) {
+            for (String permission : PERMISSIONS) {
+                if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, PERMISSIONS, READ_CONTACTS_PERMISSION);
                 }
             }
         }
-    }
 
-    public void ShowContact(View v) {
-        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(intent, SHOW_CONTACT);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == SHOW_CONTACT) {
-            if(resultCode == RESULT_OK) {
-                final String[] projection = new String[] {
-                        ContactsContract.Contacts.DISPLAY_NAME
-                };
-                Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
-                        projection, null, null, null);
-                if(cursor != null) {
-                    if(cursor.moveToFirst()) {
-                        String name = cursor.getString(
-                                cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        activityResultLaunch = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    Uri selectedContactUri = result.getData().getData();
+                    if(result.getResultCode() == RESULT_OK) {
+                        final String[] projection = new String[] { ContactsContract.Contacts.DISPLAY_NAME };
+                        Cursor cursor = getContentResolver().query(selectedContactUri,
+                                projection, null, null, null);
+                        if(cursor != null) {
+                            if(cursor.moveToFirst()) {
+                                int columnIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                                String name = cursor.getString(columnIndex);
+                                Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
+                            }
+                            cursor.close();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "User cancel", Toast.LENGTH_LONG).show();
                     }
-                }
-            } else {
-                Toast.makeText(this, "User canceled", Toast.LENGTH_SHORT).show();
-            }
-        }
+                });
+
+        showContact = findViewById(R.id.btnShowContact);
+        showContact.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+            activityResultLaunch.launch(intent);
+        });
     }
 }
